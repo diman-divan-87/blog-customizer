878b9c05bd7571bd16e638c2e3c2ae69286f0344
DIMA <Pecherskiy@vector-best.ru>
2025-11-19 11:10:43 +0700

    Открытие - закрытие сайд-бар

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,27 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpenedSideBar, setIsOpenedSideBar] = useState(false);
+	const toggleSideBar = (isOpenedSideBar: boolean) => {
+		setIsOpenedSideBar(!isOpenedSideBar);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpenedSideBar}
+				onClick={() => {
+					toggleSideBar(isOpenedSideBar);
+				}}
+			/>
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpenedSideBar,
+				})}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
