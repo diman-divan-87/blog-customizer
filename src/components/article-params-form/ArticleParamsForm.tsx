b05dd0639458f86f0c57ad264512306776c2a30d
DIMA <Pecherskiy@vector-best.ru>
2025-11-19 11:34:06 +0700

    Добавлены элементы в сайд-бар

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,12 +4,22 @@ import { Button } from 'src/ui/button';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
 import clsx from 'clsx';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
 	const [isOpenedSideBar, setIsOpenedSideBar] = useState(false);
 	const toggleSideBar = (isOpenedSideBar: boolean) => {
 		setIsOpenedSideBar(!isOpenedSideBar);
 	};
+	const options = [
+		{ title: '1 опция', value: '1 опция', className: '' },
+		{ title: '2 опция', value: '2 опция', className: '' },
+		{ title: '3 опция', value: '3 опция', className: '' },
+		{ title: '4 опция', value: '4 опция', className: '' },
+	];
+	const [selected, setSelected] = useState(options[0]);
 
 	return (
 		<>
@@ -24,6 +34,45 @@ export const ArticleParamsForm = () => {
 					[styles.container_open]: isOpenedSideBar,
 				})}>
 				<form className={styles.form}>
+					<Select
+						selected={selected}
+						onChange={setSelected}
+						options={options}
+						title='Название выпадающего списка'
+					/>
+					<RadioGroup
+						name={'radioGroupName'}
+						options={options}
+						selected={selected}
+						onChange={setSelected}
+						title='размер шрифта'
+					/>
+
+					<Select
+						selected={selected}
+						onChange={setSelected}
+						options={options}
+						title='Название выпадающего списка'
+					/>
+					<br />
+					<br />
+					<Separator />
+					<br />
+					<br />
+					<Select
+						selected={selected}
+						onChange={setSelected}
+						options={options}
+						title='Название выпадающего списка'
+					/>
+
+					<Select
+						selected={selected}
+						onChange={setSelected}
+						options={options}
+						title='Название выпадающего списка'
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
